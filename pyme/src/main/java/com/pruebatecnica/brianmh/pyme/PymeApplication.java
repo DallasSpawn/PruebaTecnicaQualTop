1af612ac6fbe8a7ce91380fe85681cf1a5f3e215
Medrano Brian <67834746+DallasSpawn@users.noreply.github.com>
2026-08-13 09:50:14 -0600

    Initial commit

---
+++ pyme/src/main/java/com/pruebatecnica/brianmh/pyme/PymeApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.brianmh.pyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymeApplication.class, args);
+	}
+
+}
